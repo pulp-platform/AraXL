b0afa2d5cf3362498711415e8d6c756c3aab0f5b
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2026-02-09 00:11:37 +0100

    [apps] add misaligned vector load regression tests

--- apps/rvv-tests/vle16.c
+++ apps/rvv-tests/vle16.c
@@ -270,6 +270,37 @@ void TEST_CASE18(void) {
   LVCMP_U16(18, v12, LONG_I16);
 }
 
+//**** VL regression and misalignment load tests */
+
+// Test cases to load irregular vector lengths and misaligned start address
+uint16_t res[130] __attribute__((aligned(AXI_DWIDTH)));
+void TEST_CASE19(void) {
+  printf("Running vl regression tests with misaligned address\n");
+  int avl, vl;
+  int misalign = 3;
+
+  int vl_list[12] = {4, 8, 5, 9, 16, 17, 20, 21, 32, 33, 128, 129};
+
+  for (int i=0; i<12; i++) {
+    int vl = vl_list[i];
+    printf("vl=%d\n", vl);
+
+    asm volatile("vsetvli %0, %1, e16, m8, ta, ma" : "=r"(avl) : "r"(vl));
+    asm volatile("vle16.v v8, (%0)"::"r"(&LONG_I16[misalign]));
+    asm volatile("vse16.v v8, (%0)"::"r"(&res));
+
+    for (int idx=0; idx<vl; idx++) {
+      if (res[idx]!=LONG_I16[idx+misalign]) {
+        printf("Index error at idx:%d expected:%hd got:%hd\n",idx,LONG_I16[idx+misalign],res[idx]);
+        return;
+      }
+    }
+
+    printf("PASSED.\n");
+  }
+
+}
+
 int main(void) {
   INIT_CHECK();
   enable_vec();
@@ -293,6 +324,8 @@ int main(void) {
   TEST_CASE16();
   TEST_CASE17();
   TEST_CASE18();
+
+  TEST_CASE19();
 
   EXIT_CHECK();
 }
