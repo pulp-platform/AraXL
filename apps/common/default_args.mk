f6d8e8498b21c4f11535a9e4408d8208bb348802
Navaneeth-Kunhi Purayil <nkunhi@iis.ee.ethz.ch>
2026-07-25 13:57:43 +0200

    [apps] fix iconv2d kernel

--- apps/common/default_args.mk
+++ apps/common/default_args.mk
@@ -3,8 +3,8 @@
 # Matrix sizes
 def_args_imatmul     ?= "16 64 128"
 def_args_fmatmul     ?= "16 64 128"
-# Matrix size, filter size
-def_args_iconv2d     ?= "112 7"
+# Matrix rows, matrix cols, filter size
+def_args_iconv2d     ?= "112 112 7"
 def_args_fconv2d     ?= "64 64 7"
 def_args_fconv3d     ?= "112 7"
 # Vector size
